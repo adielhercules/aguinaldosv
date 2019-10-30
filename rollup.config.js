480abc24ec76cf40fc36e3141180133b8af59fca
Adiel Hercules <jadher.11x2@gmail.com>
2019-10-30 16:42:07 -0600

    Include js for looking up classes before purge

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import postcss from 'rollup-plugin-postcss';
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
-  content: ['./public/**/*.html', './public/**/*.js'],
+  content: ['./public/**/*.html', './src/**/*.js'],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
