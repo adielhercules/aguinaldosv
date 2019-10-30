061b7351f5aa982c27836e89317d5ecca632af57
Adiel Hercules <jadher.11x2@gmail.com>
2019-10-30 00:06:17 -0600

    Add period calculation

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -3,24 +3,33 @@
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
   }
 
-  const now = new Date();
-  const currentYear = now.getFullYear();
+  var now = new Date();
+  var currentYear = now.getFullYear();
 
   function noOffsetDate(d) {
     return new Date(d.getTime() + d.getTimezoneOffset() * 60 * 1000);
   }
 
-  const nextBonusDate = noOffsetDate(new Date(`${currentYear}-12-12`));
+  var nextBonusDate = noOffsetDate(new Date(`${currentYear}-12-12`));
 
-  function getAguinaldo(salary, hiringDate) {
+  function getNumberOfDaysInPeriod(period) {
+    switch (period) {
+      case 'quincenal':
+        return 15;
+      default:
+        return 30;
+    }
+  }
+
+  function getAguinaldo(salary, hiringDate, period) {
     // Constants
-    let days = 0;
-    const oneDay = 1000 * 60 * 60 * 24;
-    const noOfDays = Math.ceil((nextBonusDate.getTime() - hiringDate.getTime()) / oneDay);
+    var days = 0;
+    var oneDay = 1000 * 60 * 60 * 24;
+    var noOfDays = Math.ceil((nextBonusDate.getTime() - hiringDate.getTime()) / oneDay);
+    var numOfDaysInPeriod = getNumberOfDaysInPeriod(period);
 
     if (noOfDays <= 365) {
-      const p = (noOfDays * 100) / 365 / 100;
-      days = 15 * p;
+      days = 15 * ((noOfDays * 100) / 365 / 100);
     } else if (noOfDays > 365 && noOfDays < 365 * 3) {
       days = 15;
     } else if (noOfDays > 365 * 3 && noOfDays < 3650) {
@@ -29,27 +38,37 @@
       days = 21;
     }
 
-    const dailySalary = salary / 30;
+    var dailySalary = salary / numOfDaysInPeriod;
 
-    const aguinaldo = dailySalary * days;
+    var aguinaldo = dailySalary * days;
 
     return currency(aguinaldo.toFixed(2));
+  }
+
+  function iterateClassList(classesArray, element, action) {
+    classesArray.forEach(function addClassName(className) {
+      element.classList[action](className);
+    });
   }
 
   function initialize() {
     var form = global.document.getElementById('calculateAguinaldo');
     var salaryField = form.elements.salary;
     var dateField = form.elements.date;
+    var periodField = form.elements.periodo;
     var alert = global.document.getElementById('alert-no-date');
     var submitBtn = global.document.getElementById('submitBtn');
     var result = global.document.getElementById('result');
+    var activePeriodClasses = ['bg-green-700', 'hover:bg-green-600'];
+    var inactivePeriodClasses = ['bg-xmas', 'hover:bg-green-900'];
 
     function onSubmit(e) {
       var salary = salaryField.value;
       var date = dateField.valueAsDate;
+      var periodo = periodField.value;
       var emptySalary = String(salary).length === 0;
       var hiringDate = date && date < nextBonusDate ? date : null;
-      var aguinaldo = getAguinaldo(hiringDate ? salary : 0, hiringDate || now);
+      var aguinaldo = getAguinaldo(hiringDate ? salary : 0, hiringDate || now, periodo);
 
       e.preventDefault();
 
@@ -74,12 +93,32 @@
       alert.classList.add('hidden');
     }
 
+    function onChangePeriod() {
+      var periodo = periodField.value;
+
+      periodField.forEach(function eachPeriodField(field) {
+        iterateClassList(activePeriodClasses, field.parentElement, 'remove');
+        iterateClassList(inactivePeriodClasses, field.parentElement, 'remove');
+      });
+
+      iterateClassList(
+        activePeriodClasses,
+        periodField[periodo === 'mensual' ? 0 : 1].parentElement,
+        'add',
+      );
+    }
+
     dateField.max = now.toISOString().split('T')[0];
+    iterateClassList(activePeriodClasses, periodField[0].parentElement, 'add');
+    iterateClassList(inactivePeriodClasses, periodField[1].parentElement, 'add');
 
     form.addEventListener('submit', onSubmit);
     dateField.addEventListener('change', onChange);
     salaryField.addEventListener('change', onChange);
     submitBtn.addEventListener('click', onSubmit);
+    periodField.forEach(function eachPeriodField(field) {
+      field.addEventListener('change', onChangePeriod);
+    });
   }
 
   global.document.addEventListener('DOMContentLoaded', initialize);
